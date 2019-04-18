11b4f94dd9519c1cc63fb126441f9d6a0b4cf991
Fernando Torresan de Oliveira <fernandotdo61@gmail.com>
2019-04-17 23:37:19 -0300

    Improvement in resource instance

--- modules/single-server/main.tf
+++ modules/single-server/main.tf
@@ -2,46 +2,47 @@
 #
 # ----------------------------------------------------------------------------------------------------------------------
 
-resource "aws_instance" "" {
-  ami           = ""
-  instance_type = "t2.micro"
-  key_name      = "${var.key_name}"
+variable "ami" {
+  description = "The AMI to use for the instance."
+  default     = ""
+}
 
-  block_device {}
+locals {
+  ami = "${var.ami != "" ? var.ami : data.aws_ami.default.image_id}"
+}
 
-  ebs_block_device {
-    device_name = ""
-    volume_type = ""
-    volume_size = ""
+data "aws_ami" "default" {
+  most_recent = "true"
 
-    delete_on_termination = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 
-  connection {}
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "main" {
+  instance_type   = "${var.instance_type}"
+  key_name        = "${var.key_pair_name}"
+  ami             = "${data.aws_ami.default.image_id}"
+  security_groups = "${var.security_groups}"
+
+  vpc_security_group_ids = "${var.vpc_security_group_ids}"
+  subnet_id              = "${var.subnet_id}"
+
+  root_block_device {
+    volume_type           = "${var.root_volume_type}"
+    volume_size           = "${var.root_volume_size}"
+    delete_on_termination = "${var.delete_on_termination}"
+  }
 
   tags {
     Name = "${var.instance_name}"
   }
-}
-
-resource "aws_security_group" {
-  ingress {
-    from_port = 0
-    protocol  = ""
-    to_port   = 0
-  }
-
-  egress {
-    from_port = 0
-    protocol  = ""
-    to_port   = 0
-  }
-}
-
-resource "aws_security_group_rule" "" {
-  from_port         = 0
-  protocol          = ""
-  security_group_id = ""
-  to_port           = 0
-  type              = "egress"
 }
