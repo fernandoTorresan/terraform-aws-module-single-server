5d023d68b8ca56d7a82a78d7607efb57556b2aa5
Fernando Torresan de Oliveira <fernandotdo61@gmail.com>
2019-04-17 23:36:20 -0300

    Some variables used into instance

--- example/single-server/main.tf
+++ example/single-server/main.tf
@@ -1,1 +1,7 @@
+module "ec2" {
+  source = "../../modules/single-server"
 
+  instance_name          = "simple-server"
+  key_pair_name          = "fernando-torresan"
+  vpc_security_group_ids = ["sg-000fb47d29a4975ff"]
+}
--- modules/single-server/variables.tf
+++ modules/single-server/variables.tf
@@ -1,3 +1,44 @@
 variable "instance_name" {
   description = "Name of the server"
 }
+
+variable "root_volume_type" {
+  description = "The type of volume. Can be 'standard', 'gp2', 'io1', 'sc1' ou 'st1'. "
+  default     = "standard"
+}
+
+variable "root_volume_size" {
+  description = "The size of the volume in gibibytes."
+  default     = 8
+}
+
+variable "delete_on_termination" {
+  description = "If the volume should be destroyed on instance termination."
+  default     = true
+}
+
+variable "subnet_id" {
+  description = "The VPC Subnet ID to launch in."
+  default     = ""
+}
+
+variable "vpc_security_group_ids" {
+  description = "A list of security group IDs to associate with."
+  type        = "list"
+  default     = []
+}
+
+variable "security_groups" {
+  description = "A list of security group names or IDs to associate with."
+  type        = "list"
+  default     = []
+}
+
+variable "key_pair_name" {
+  description = "Name of key pair to access via SSH"
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "Type of instance to be created"
+}
